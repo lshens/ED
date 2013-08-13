4850e0d1567419a44dbe476c6e2ab0bc127364ea
lucas.shen <lshens@hotmail.com>
2013-08-13 16:24:09 -0300

    SEGUNDO COMMIT LISTA DE ED!

--- Recursao/somadivruim.py
+++ Recursao/somadivruim.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-
 __author__ = 'lucas.shen'
 #EXERCICIO 6 LISTA RECURSÃO
-def f(x, y):
+def fr(x, y):
+    #print(x, '|', y)
     if x >= y:
-        return float((x + y) / 2)
+        return float((x + y)/2)
     else:
-        return float(f(f(x + 2, y - 1), f(x + 1, y - 2)))
+        return float(fr(fr(x + 2, y - 1), fr(x + 1, y - 2)))
 
+def fb(x, y):
+    #print(x, '|', y)
+    if x >= y:
+        return float((x + y)/2)
+    else:
+        return float(fb(x + 2.5, y - 2.5))
 
-print(f(1, 10))
+print(fr(3, 10), 'F - RUIM')
+print(fb(3, 10), 'F-BOM')
