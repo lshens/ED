777aca6b6f0676a6adeef254f96449426f36999d
lucas.shen <lshens@hotmail.com>
2013-10-31 16:00:34 -0200

    QUINTO COMMIT EP2

--- Problema do Rei Arthur/Merlin.py
+++ Problema do Rei Arthur/Merlin.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from operator import itemgetter
+
 def enumeracoes(items):
     n = len(items)
     s = [0]*(n+1)
@@ -30,20 +32,46 @@ def permutacoes(items):
 
 def arquivo(url):
     arq = open(url)
-    txt = arq.readlines()
+    txt = arq.readlines() #Cria uma lista pra cada linha do arquivo
     aux_d = 0
     dic = {}
-    for i in txt:
+    for i in txt: #Pra cada linha cria um valor no dicionario
         linha = txt[aux_d].split()
-        dic[linha[0]] = linha[1:]
+        dic[linha[0]] = linha[1:] #O dicionario recebe como chave a pessoa que possue favoritos e os valores são os favoritos
         aux_d += 1
     arq.close()
-    return dic.values()
+    return dic
 
-#print ('Permutações')
-#for p in permutacoes(['Adriano','Bruno', 'Diogo', 'Eclis', 'Gabriel', 'Leandro', 'Walber']):
-#    print (p)
+def casamento(damasEPreferidos):
+    casado = []
+    for i in damasEPreferidos:
+        j = 0
+        qtdCavaleiros = len(damasEPreferidos[i])
+        if qtdCavaleiros == 1: #Verifica se temos apenas um pretendente
+            if damasEPreferidos[i][j] not in casado: #Caso o pretende não esteja na lista, casa ele
+                casado.append(damasEPreferidos[i][j])
+            else: #Se tiver na lista quer dizer q não tem como casar, pq só tem 1 pretendente
+                print("Casamento não é possivel, ")
+                break
+        while j < qtdCavaleiros: #Enquanto tiver cavaleiros ele roda
+            if damasEPreferidos[i][j] not in casado: #Caso o pretende não esteja na lista, casa ele e quebra o WHILE
+                casado.append(damasEPreferidos[i][j])
+                break
+            j += 1
+    return "Casamento possivel, "
 
-print ('Enumerações')
-for p in enumeracoes(arquivo("C:\Users\lucas.shen\PycharmProjects\EstruturaDados\Problema do Rei Arthur\casamento.txt")):
-    print (p)
+#Chamadas dos metodos
+dicCasamento = arquivo("C:\Users\lucas.shen\PycharmProjects\EstruturaDados\Problema do Rei Arthur\casamento.txt")
+cavaleirosEPreferidos = arquivo("C:\Users\lucas.shen\PycharmProjects\EstruturaDados\Problema do Rei Arthur\cavaleiros.txt")
+cavaleiros = cavaleirosEPreferidos.keys()
+casamento(dicCasamento)
+
+for p in permutacoes(cavaleiros):
+    if p[1] in cavaleirosEPreferidos[p[0]] and p[2] in cavaleirosEPreferidos[p[1]] \
+        and p[3] in cavaleirosEPreferidos[p[2]] and p[4] in cavaleirosEPreferidos[p[3]] \
+        and p[5] in cavaleirosEPreferidos[p[4]] and p[6] in cavaleirosEPreferidos[p[5]] \
+        and p[0] in cavaleirosEPreferidos[p[6]]:
+        print("Mesa: %s" % p[0:7])
+        break
+    else:
+        continue
