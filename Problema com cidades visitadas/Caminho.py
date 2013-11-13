3648e62e61e62da340597e1719e27d0f45592969
Shen <lshens@hotmail.com>
2013-11-12 22:50:33 -0200

    QUARTO COMMIT EP3

--- Problema com cidades visitadas/Caminho.py
+++ Problema com cidades visitadas/Caminho.py
@@ -16,22 +16,24 @@ def imprimiMatriz(matriz):
     for i in range(len(matriz)):
         aux = ''
         for j in range(len(matriz[i])):
-            aux = aux + str(matriz[i][j]) + ' '
+            aux = aux + str(matriz[i][j])
         print(aux)
 
 def percorre(matriz):
     #A matriz é varrida de cima(linha[0]) para baixo(linha[n]) e
     #da esquerda(coluna[0] para a direita[n]
+    aux = []
+    cont = 1
     for linha in range(len(matriz)):
         for coluna in range(len(matriz)):
-            #Se um "pixel" tem valor 1, verificamos seu vizinho de cima e da esu=querda
+            #Se um "pixel" tem valor 1, verificamos seu vizinho de cima e da esquerda
             if matriz[linha][coluna] == 1:
                 #Se ambos são ZERO, um novo objeto começa neste pixel
-                if matriz[linha -1][coluna] and matriz[linha][coluna + 1] == 0:
-                    return 'Ainda não sei oq fazer aqui'
+                if matriz[linha -1][coluna] and matriz[linha][coluna - 1] == 0:
+                    aux.append(matriz[linha][coluna])
                 #Se um deles é 1, este pixel pertence ao objeto anterior
-                if matriz[linha -1][coluna] or matriz[linha][coluna + 1] == 1:
-                    return 'Ainda não sei oq fazer aqui'
+                if matriz[linha -1][coluna] or matriz[linha][coluna - 1] == 1:
+                    aux.append(matriz[linha][coluna])
 
 t = '''101
        111
