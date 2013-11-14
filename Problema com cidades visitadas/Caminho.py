f11d546055a36f8138a895cd186a8fc45c88fd52
lucas.shen <lshens@hotmail.com>
2013-11-14 16:19:22 -0200

    FINAL COMMIT EP3

--- Problema com cidades visitadas/Caminho.py
+++ Problema com cidades visitadas/Caminho.py
@@ -1,42 +1,33 @@
 # -*- coding: utf-8 -*-
 
-def geraMatriz(tupla):
-    lista = tupla.split()
-    #Variaveis auxiliares
-    linhaMatriz = []
-    matriz = []
-    for linha in range(len(lista)): #percorre pelo numero de linhas
-        for coluna in range(len(lista[0])): #percorre pelo numero de colunas
-            linhaMatriz.append(lista[linha][coluna]) #adiciona na lista
-        matriz.append(linhaMatriz)
-        linhaMatriz = [] #zera a lista para uma nova linha
-    return matriz
 
-def imprimiMatriz(matriz):
-    for i in range(len(matriz)):
-        aux = ''
-        for j in range(len(matriz[i])):
-            aux = aux + str(matriz[i][j])
-        print(aux)
 
 def percorre(matriz):
     #A matriz é varrida de cima(linha[0]) para baixo(linha[n]) e
     #da esquerda(coluna[0] para a direita[n]
     aux = []
-    cont = 1
+    cont = 0
     for linha in range(len(matriz)):
         for coluna in range(len(matriz[0])):
-            print(matriz[linha][coluna])
+          #  print(matriz[linha][coluna])
             #Se um "pixel" tem valor 1, verificamos seu vizinho de cima e da esquerda
-            if matriz[linha][coluna] == 1:
-                #Se ambos são ZERO, um novo objeto começa neste pixel
-                if matriz[linha -1][coluna] and matriz[linha][coluna - 1] == 0:
+            if '1' in matriz[linha][coluna]:
+                #Se ambos são ZERO, um novo objeto começa neste pixel(Um novo regioes)
+                if matriz[linha -1][coluna] and matriz[linha][coluna - 1] == '0':
                     aux.append(matriz[linha][coluna])
                 #Se um deles é 1, este pixel pertence ao objeto anterior
-                if matriz[linha -1][coluna] or matriz[linha][coluna - 1] == 1:
-                    aux.append(matriz[linha][coluna])
+                if matriz[linha -1][coluna] == '1' or matriz[linha][coluna - 1] == '1':
+                    aux.extend(matriz[linha][coluna])
+            else:
+                aux.append(matriz[linha][coluna])
+            #Se ambos os vizinhoes tem rotulos diferentes, a matriz devera ser corrigida para que
+            #ambos tenham o mesmo rotulo ( ou seja, um dos rotulos vai desaparecer e todos os pixels daquele
+            #rotulo vão passar a ter outro)
+    print(aux)
 
-t = '''101
+
+
+t = '''010
        111
        000
        101'''
@@ -44,6 +35,6 @@ t = '''101
 #print("Entrada:")
 #imprimiMatriz(geraMatriz(t))
 
-print(percorre(geraMatriz(t)))
+percorre(geraMatriz(t))
 
 
--- Problema com cidades visitadas/Cidades.py
+++ Problema com cidades visitadas/Cidades.py
@@ -1,65 +1,76 @@
 # -*- coding: utf-8 -*-
-# Utlização de uma matriz de números, onde verificará e imprimirá regiões existentes
-# Apenas verificará os 1(s) que estão somente na horizontal e vertical
+#EP3 Lucas Shen e Paloma Ribeiro
+def geraMatriz(tupla):
+    lista = tupla.split()
+    #Variaveis auxiliares
+    linhaMatriz = []
+    matriz = []
+    for linha in range(len(lista)): #percorre pelo numero de linhas
+        for coluna in range(len(lista[0])): #percorre pelo numero de colunas
+            linhaMatriz.append(int(lista[linha][coluna])) #adiciona na lista
+        matriz.append(linhaMatriz)
+        linhaMatriz = [] #zera a lista para uma nova linha
+    print(matriz)
+    return matriz
 
-matriz = [[0,1,0],
-	    [1,1,1],
-        [0,0,0],
-	    [1,0,1]]
+def imprimiMatriz(matriz):
+    for i in range(len(matriz)):
+        saida = ''
+        for j in range(len(matriz[i])):
+            saida = saida + str(matriz[i][j])
+        print(saida)
 
-'''[[0,1,0,0,0,0],
-          [0,0,1,0,0,0],
-          [0,0,0,0,1,0],
-          [0,0,1,0,1,0],
-          [1,0,0,0,0,0],
-          [0,1,0,0,0,0]]'''
+#t = '''010
+#       111
+#       000
+#       101'''
 
-'''
-[[0,1,1,0,0,0,1,0,1,0],
-[0,0,1,1,0,0,1,1,1,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,1,0,0,0,1,0,1,0],
-[0,0,1,0,0,1,1,1,1,1],
-[1,1,1,1,1,0,0,0,0,0],
-[0,0,1,0,0,0,1,1,1,0],
-[0,0,1,0,0,0,1,1,1,0],
-[0,0,1,1,1,0,0,0,1,0]]'''
+#t = '''10101
+#       10101
+#       11111'''
 
-def varre( i, j, cont ):
-    if( matriz[i][j] != 999 ):#Se for 0 faz nada
+t = '''0011001010
+       0110001010
+       0011001110
+       0000000000
+       0010001010
+       0010011111
+       1111100000
+       0010001110
+       0010001110'''
+
+matriz = geraMatriz(t)
+
+def verificaVizinho(i, j, cont):
+    if(matriz[i][j] != 999):
         return()
-    matriz[i][j] = cont# Troca 999 pelo valor de cont
+    matriz[i][j] = cont
 
-    #Aqui está verificando se pode varrer o elemento
-
-    if( len( matriz[i] ) > j + 1 ):    # da direita  
-        varre( i, j + 1, cont )                   
-    if( len( matriz ) > i + 1 ):       #de cima
-        varre( i + 1, j, cont )
-    if( j - 1 >= 0 ):                  #da esquerda
-        varre( i, j - 1, cont )
-    if( i - 1 >= 0 ):                  #o debaixo
-        varre( i - 1, j, cont )
+    if(len(matriz[i]) > j + 1):
+        verificaVizinho(i, j + 1, cont)
+    if(len(matriz) > i + 1):
+        verificaVizinho(i + 1, j, cont)
+    if(j - 1 >= 0):
+        verificaVizinho(i, j - 1, cont)
+    if(i - 1 >= 0):
+        verificaVizinho(i - 1, j, cont)
     return()
 
-#Percorre cada elemento da matriz e subistitui por 999
-for i in range( len( matriz ) ): #linha
-    for j in range( len( matriz[i] ) ): #coluna
-        if( matriz[i][j] == 1 ):
+print('Entrada:')
+imprimiMatriz(matriz)
+
+for i in range(len(matriz)): #linha
+    for j in range(len(matriz[i])): #coluna
+        if(matriz[i][j] == 1):
             matriz[i][j] = 999
 
 cont = 1
-# Varrerá a matriz, chamando a funçao de troca para cd posiçao com 999
-for i in range( len( matriz ) ):
-    for j in range( len( matriz[i] ) ):
-        varre( i,j, cont)
-        if ( matriz[i][j] == cont ):
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        verificaVizinho( i,j, cont)
+        if (matriz[i][j] == cont):
             cont += 1
 
-# aqui se encarregará de mostrar as regiões, com sua devidas regiões
-print( "\nMatriz depois: ")
-for i in range( len( matriz ) ):#laço que imprimirá a matriz formatada (depois)
-    matrix = ''
-    for j in range( len( matriz[i] ) ):
-        matrix = matrix + str( matriz[i][j] ) + ' '
-    print( matrix )
+print('Saída:')
+imprimiMatriz(matriz)
